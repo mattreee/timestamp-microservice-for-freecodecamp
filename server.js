4b30d0781df06c67a42e70470db321f4b9dac681
mattreee <fantinatomatheuss@gmail.com>
2022-04-08 17:18:18 -0400

    added api endpoint

--- server.js
+++ server.js
@@ -8,25 +8,83 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+function validatorMiddleware(request, response, next) {
+   const { time } = request.params;
+   const hasLettersRegExp = /[a-z]/gi;
+   const hasDashRegExp = /\-/gi;
+   const UTCformatRegExp = /^([0-9]{4,})-(0[1-9]{1}|1[012]{1})-((0[1-9]{1}|(1|2)[0-9]{1})|(3[01]))$/;
+
+   if (hasLettersRegExp.test(time)) {
+      return response
+         .status(400)
+         .json({ error: 'date provided must not contain letters' });
+   };
+
+   if (hasDashRegExp.test(time)) {
+      if (time.length !== 10) {
+         return response
+            .status(400)
+            .json({ error: 'UTC time provided is not valid' });
+      };
+
+      if (!UTCformatRegExp.test(time)) {
+         return response
+            .status(400)
+            .json({ error: 'Invalid date. UTC dates must follow YYYY-MM-DD format' });
+      }
+   };
+
+
+   if (time === undefined) {
+      return response
+         .status(400)
+         .json({ error: 'user must inform a date or unix time' });
+   };
+
+   request.time = time;
+   next();
+}
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+   res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/:time?', validatorMiddleware, (request, response) => {
+   const { time } = request;
+
+   let result = {
+      unix: "",
+      utc: ""
+   };
+
+   if (time.length === 10 && time.indexOf('-') !== -1) {
+      result.unix = new Date(time).getTime();
+      result.utc = new Date(time).toUTCString();
+   } else {
+      result.unix = new Date(Number(time)).getTime();
+      result.utc = new Date(Number(time)).toUTCString();
+   }
+
+   return response.json(result);
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const PORT = process.env.PORT || 3333;
+
+var listener = app.listen(PORT, function () {
+   console.log('Your app is listening on port ' + listener.address().port);
 });
